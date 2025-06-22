e107c9815ded9c4c18fd4bbbb85c1555b1fd2883
Ali-Moosa <newlevel2119@gmail.com>
2025-06-22 15:34:24 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+let hide = document.getElementById("hide");
+let nav = document.querySelector(".nav-contain");
+let block = document.querySelector(".block");
+let heros = document.querySelector(".heros-imgs");
+let logo = document.querySelector(".logo");
+let left = document.querySelector(".left-one");
+let section1 =document.querySelector(".sec1");
+
+hide.addEventListener("click", ()=>{
+    nav.style.display="none";
+    block.style.display="block";
+    heros.style.display="block";
+    left.style.display="block";
+});
+
+block.addEventListener("click", () => {
+    nav.style.display="block";
+    block.style.display="none";
+});
+
+hide.addEventListener("click",()=>{
+    if (window.matchMedia("(max-width: 530px)").matches) {
+        logo.style.display="block";
+        logo.style.marginLeft="110px";
+    }
+});
+
+block.addEventListener("click",()=>{
+    if (window.matchMedia("(max-width: 530px)").matches) {
+        nav.style.display="block";
+        block.style.display="none";
+        heros.style.display="none";
+        left.style.display="none";
+        logo.style.display="none";
+    }
+    })
